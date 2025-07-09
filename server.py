f3aae64a6dff7fdb9bc35f4a63df16d02b9fb086
xKZNGG <chefezinhokaua11@gmail.com>
2025-07-09 07:55:35 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+clients = []
+
+@app.websocket("/mic")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+            for client in clients:
+                if client != websocket:
+                    await client.send_bytes(data)
+    except:
+        clients.remove(websocket)
